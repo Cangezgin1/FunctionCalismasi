fb3a97a24a5221bc966d057b032300648c39c4b8
CanGezgin <can.gesgin@gmail.com>
2021-11-26 20:13:53 +0300

    ilk commit

---
+++ main.js
@@ -0,0 +1,16 @@
+function İşle(){
+    var aa=Number(document.getElementById('ilksayı').value);
+    var bb=Number(document.getElementById('ikincisayı').value);
+    var toplamı=aa+bb;
+    var çarpma=aa*bb;
+    var bölme=aa/bb;
+    console.log("Toplamı;");
+    console.log(toplamı);
+    console.log("Çarpımı;");
+    console.log(çarpma);
+    console.log("Bölme;");
+    console.log(bölme);
+}
+function Cagırma(){
+    İşle();
+}
