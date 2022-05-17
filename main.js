95e945d6cc6affae64f24f2a526b61075584fdf9
Muhammed Can GEZGİN <93928479+Cangezgin1@users.noreply.github.com>
2022-05-17 15:57:44 +0300

    Küçük düzenlemeler yapıldı.

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function İşle(){
-    var aa=Number(document.getElementById('ilksayı').value);
-    var bb=Number(document.getElementById('ikincisayı').value);
-    var toplamı=aa+bb;
-    var çarpma=aa*bb;
-    var bölme=aa/bb;
+    var sayi1=Number(document.getElementById('ilksayı').value);
+    var sayi2=Number(document.getElementById('ikincisayı').value);
+    var toplamı=sayi1+sayi2;
+    var çarpma=sayi1*sayi2;
+    var bölme=sayi1/sayi2;
     console.log("Toplamı;");
     console.log(toplamı);
     console.log("Çarpımı;");
